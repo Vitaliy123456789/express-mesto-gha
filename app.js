26cff74fc6c915f97cf794c0dde2af9cbf3adcf9
Vitaliy <im.nikitin.vitaliy@yandex.ru>
2023-09-04 10:38:28 +0300

    12

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().default('Жак-Ив Кусто').min(2).max(30),
     about: Joi.string().default('Исследователь').min(2).max(30),
-    avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
+    avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png').pattern(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
